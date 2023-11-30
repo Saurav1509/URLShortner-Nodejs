8bb34118c17328800aeb1cfb0cd42597a9042c8a
Saurav1509 <Saurav1509@users.noreply.github.com>
2023-11-30 17:21:21 +0530

    Solved with constance url

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-require('dotenv').config();
+//require('dotenv').config();
+const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +20,22 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+
+let originalurl;
+let r = (Math.random() + 1).toString(36).substring(7);
+
+app.post('/api/shorturl', bodyParser.urlencoded(), (req,res) =>{
+  console.log(req.body.url)
+  originalurl = req.body.url;
+  r = (Math.random() + 1).toString(36).substring(7);
+  res.json({ original_url : originalurl, short_url : 1});
+});
+
+app.get(`/api/shorturl/1`, (req, res) => {
+  console.log(originalurl);
+  res.redirect(originalurl);
 });
 
 app.listen(port, function() {
